6522bd5cfd2fc2db24cd325b3f9b996b9ffe470a
iamxiaoma <395487290@qq.com>
2020-07-26 00:18:08 +0800

    added: 1、新增事务

--- src/main/java/com/veasymall/api/service/impl/UserServiceImpl.java
+++ src/main/java/com/veasymall/api/service/impl/UserServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.pagehelper.PageHelper;
 import com.mysql.cj.util.StringUtils;
@@ -20,35 +22,41 @@ public class UserServiceImpl implements UserService {
 	private UserMapper userMapper;
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void saveUser(User user) throws Exception {
 
 		userMapper.insert(user);
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void updateUser(User user) {
 		userMapper.updateByPrimaryKeySelective(user);
 
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void deleteUser(Integer userId) {
 		userMapper.deleteByPrimaryKey(userId);
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public User queryUserById(Integer userId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public List<User> queryUserListPagedJqgrid(User user, Integer page, Integer pageSize) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public List<User> queryUserListPaged(User user, Integer page, Integer pageSize) {
 
 		// 开始分页
